44deb2942cd1bcc764b1279b73932971ddac4b30
Rafael Reyes <raffeee.reyes@gmail.com>
2022-10-14 23:19:53 +0800

    Complete exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  const celsius = (temp - 32) * 5/9;
+  return parseFloat(celsius.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  const fahrenheit = (temp * 9/5 + 32)
+  return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
