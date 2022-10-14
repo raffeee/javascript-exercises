3138d5db9d0b2eb57d25a264cd8ff4f4310cdd5e
Rafael Reyes <raffeee.reyes@gmail.com>
2022-10-14 20:55:25 +0800

    Complete exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, repeatTimes) {
+  if (repeatTimes < 0) return 'ERROR';
+  let repeated = '';
+  // Nothing to repeat on empty strings, better return early
+  if (!str) return repeated;
 
+  for (let i = 0; i < repeatTimes; i++) {
+    repeated += str;
+  }
+  return repeated;
 };
 
 // Do not edit below this line
