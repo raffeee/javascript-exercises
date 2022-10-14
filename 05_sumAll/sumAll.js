32769f64391f79e29c704ecdc5c00145502237d7
Rafael Reyes <raffeee.reyes@gmail.com>
2022-10-14 22:56:44 +0800

    Complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  if (num1 < 0 || num2 < 0 || typeof num1 != "number" || typeof num2 != "number") return "ERROR";
+  const start = Math.min(num1, num2);
+  const end = Math.max(num1, num2);
+  let sum = 0;
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
