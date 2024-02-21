63e8e7a3a3a2174f480957ce08e074a937d6d005
Rafael Reyes <100984393+raffeee@users.noreply.github.com>
2024-02-21 10:27:11 +0800

    feat(08): complete calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, num) => total += num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((total, num) => total *= num, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  const values = []
+  for (let i = num; i > 0; i--) {
+    values.push(i);
+  }
+    return values.reduce((total, num) => total *= num, 1);
 };
 
 // Do not edit below this line
