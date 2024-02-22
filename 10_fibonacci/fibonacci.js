c813cbcecec1bad16279b67c77015beefd5e9369
Rafael Reyes <100984393+raffeee@users.noreply.github.com>
2024-02-22 13:23:59 +0800

    feat(10): complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(idx) {
+    if (idx < 0) return "OOPS";
+    
+    const arr = [1, 1];
+    // subtract 1 because JS indexes start at 0
+    for (let i = 0; i < Number(idx) - 1; i++) {
+        const prev = arr[i];
+        const curr = prev + arr[i+1]
+        arr.push(curr);        
+    }
+    return arr[Number(idx) - 1];
 };
 
 // Do not edit below this line
