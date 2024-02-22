1355f43519633c2ebe56bf6ad037394228187cd1
Rafael Reyes <100984393+raffeee@users.noreply.github.com>
2024-02-22 13:48:19 +0800

    feat(12): define base form of findTheOldest() fn

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, person) => {
+        let oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        let currAge = person.yearOfDeath - person.yearOfBirth;
+        if (currAge > oldestAge) {
+            oldest = person
+        }
+        return oldest
+    }, arr[0])
 };
 
 // Do not edit below this line
