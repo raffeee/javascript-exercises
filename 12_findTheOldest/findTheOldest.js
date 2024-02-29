e20238adac0cd4c01fed120e34017feddb053220
Rafael Reyes <100984393+raffeee@users.noreply.github.com>
2024-02-29 16:49:10 +0800

    feat(12): complete findTheOldest() fn

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,7 +1,12 @@
 const findTheOldest = function(arr) {
     return arr.reduce((oldest, person) => {
-        let oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
-        let currAge = person.yearOfDeath - person.yearOfBirth;
+        let currentDate = new Date();
+        let oldestAlive = oldest.yearOfDeath === undefined;
+        let currAlive = person.yearOfDeath === undefined;
+
+        let oldestAge = ((oldestAlive ? currentDate.getFullYear() : oldest.yearOfDeath) - oldest.yearOfBirth)
+        let currAge  = ((currAlive ? currentDate.getFullYear() : person.yearOfDeath) - person.yearOfBirth)
+
         if (currAge > oldestAge) {
             oldest = person
         }
