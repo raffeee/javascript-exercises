efc9cc3bcf2b2c6533a19ca048e671835a599163
Rafael Reyes <100984393+raffeee@users.noreply.github.com>
2024-03-15 11:27:45 +0800

    feat(13): Implement basic Caesar cipher function

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,17 @@
-const caesar = function() {
-
+const caesar = function(text, shiftAmt) {
+    const splitText = []
+    const deciphered = []
+    for (let i = 0; i < text.length; i++) {
+        splitText.push(text.charCodeAt(i))
+    }
+    for (let j = 0; j < splitText.length; j++) {
+        // check if Unicode is between codes for 'A' and 'z'
+        // TODO:: Implement wrapping
+        deciphered.push(splitText[j] >= 65 && splitText[j] <= 122
+            ? String.fromCharCode(splitText[j] + shiftAmt)
+            : String.fromCharCode(splitText[j]))
+    }
+    return deciphered.join('')
 };
 
 // Do not edit below this line
